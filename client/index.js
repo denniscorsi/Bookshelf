ac9d3031a6d7837593477d31b5649f8a2d5c689e
Dennis Corsi <denniscorsi@gmail.com>
2023-11-07 16:52:05 -0500

    Added stylesheet

--- client/index.js
+++ client/index.js
@@ -6,6 +6,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import '../index.css';
 
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />);
