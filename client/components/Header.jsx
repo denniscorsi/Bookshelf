f729c33d9dd8a4f7fbc2073b6e6a653deab421b0
Dennis Corsi <denniscorsi@gmail.com>
2023-11-09 12:26:58 -0500

    Styled Header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
+import AutoStoriesRoundedIcon from '@mui/icons-material/AutoStoriesRounded';
 
-const Header = () => <Typography variant='h1'>Bookshelf</Typography>;
-
+const Header = () => (
+  <Box display='flex' justifyContent='center' alignItems='center'>
+    <AutoStoriesRoundedIcon
+      sx={{
+        fontSize: '100px',
+        color: 'orange',
+      }}
+    />
+    <Typography
+      paddingX={6}
+      paddingBottom={2}
+      variant='h1'
+      sx={{
+        fontWeight: '700',
+      }}
+    >
+      Bookshelf
+    </Typography>
+  </Box>
+);
 export default Header;
