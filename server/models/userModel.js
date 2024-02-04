7c7b4aafa612c420fd437d602ced578b3ba020d9
Dennis Corsi <denniscorsi@gmail.com>
2024-02-04 13:56:42 -0500

    Update userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -6,6 +6,9 @@ const userSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  settings: {},
+  shelves: [],
+  next: [],
 });
 
 const User = mongoose.model('User', userSchema);
