bcededb399258f8e0d84b65dff9c8827944a8b37
Dennis Corsi <denniscorsi@gmail.com>
2024-03-31 19:17:45 -0400

    Fix issue with how the bundle.js path was being accessed from non-root endpoints

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-const path = require('path');
-const bookRouter = require('./routers/bookRouter');
-const authRouter = require('./routers/authRouter');
-require('dotenv').config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const bookRouter = require("./routers/bookRouter");
+const authRouter = require("./routers/authRouter");
+require("dotenv").config();
 
 const app = express();
 
@@ -13,27 +13,38 @@ mongoose.connect(process.env.MONGO_URI);
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  const index = path.join(__dirname, '../index.html');
+app.get("/", (req, res) => {
+  const index = path.join(__dirname, "../index.html");
+  console.log("getting file from root", index);
   return res.status(200).sendFile(index);
 });
 
-app.use('/books', bookRouter);
-app.use('/auth', authRouter);
+app.use("/books", bookRouter);
+app.use("/auth", authRouter);
 
-//serve static pages
-app.use('/build', express.static(path.join(__dirname, '../build')));
+// serve static pages
+app.use(express.static(path.join(__dirname, "../build")));
+app.use("/build", express.static(path.join(__dirname, "../build")));
 
-//global error handler
+
+// Fallback route since using React Router
+app.get("*", (req, res) => {
+  const index = path.join(__dirname, "../index.html");
+  console.log("getting file from fallback", index);
+
+  return res.status(200).sendFile(index);
+});
+
+// global error handler
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
+    log: "Express error handler caught unknown middleware error",
     status: 500,
-    message: { err: 'an error occured' },
+    message: { err: "an error occured" }
   };
   const errObj = Object.assign({}, defaultErr, err);
   console.log(errObj.log);
   res.status(errObj.status).json(errObj.message);
 });
 
-app.listen(3000, () => console.log('Server listening on 3000'));
+app.listen(3000, () => console.log("Server listening on 3000"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './client/index.js',
+  entry: "./client/index.js",
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+    publicPath: "/"
   },
 
   mode: process.env.NODE_ENV,
@@ -16,35 +17,38 @@ module.exports = {
         test: /\.jsx?/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
       },
       {
         test: /\.s?[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development',
-      template: 'index.html',
-    }),
+      title: "Development",
+      template: "index.html"
+    })
   ],
 
   devServer: {
     static: {
-      publicPath: path.join(__dirname + 'build'),
+      publicPath: path.join(__dirname + "build")
     },
     proxy: {
-      '/books': 'http://localhost:3000',
-      '/books/nyt/*': 'http://localhost:3000',
-      '/books/nyt/hardcover-fiction': 'http://localhost:3000',
-      '/auth': 'http://localhost:3000',
+      "/build": "http://localhost:3000",
+      "/books": "http://localhost:3000",
+      "/book": "http://localhost:3000",
+      "/books/nyt/*": "http://localhost:3000",
+      "/books/nyt/hardcover-fiction": "http://localhost:3000",
+      "/auth": "http://localhost:3000"
     },
-  },
+    historyApiFallback: true
+  }
 };
